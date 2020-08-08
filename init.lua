de8e6ebb4d808b3413eed7dc888001835246b8cb
CaptainKlutz <patkmccarthy@gmail.com>
2020-08-08 10:52:46 +0200

    Add adobe, mudbrick + recipes

--- init.lua
+++ init.lua
@@ -1,5 +1,13 @@
 civfoundations = {}
 
+minetest.register_node("civfoundations:adobe", {
+	description = "Adobe",
+	tiles = {"civf_adobe.png"},
+	groups = {cracky = 2, crumbly = 1, level = 1},
+	sounds = default.node_sound_dirt_defaults(),
+	node_dig_prediction = "solid_air",
+})
+
 minetest.register_node("civfoundations:cobble_plaster", {
 	description = "Cobblestone with Plaster",
 	paramtype2 = "facedir",
@@ -26,7 +34,7 @@ minetest.register_node("civfoundations:plaster_white", {
 	description = "White Plaster Block",
 	tiles = {"civf_plaster_white.png"},
 	is_ground_content = false,
-	groups = {cracky = 2, level = 1},
+	groups = {cracky = 2, crumbly = 1, level = 1},
 	sounds = default.node_sound_stone_defaults(),
 	node_dig_prediction = "solid_air",
 })
@@ -36,7 +44,7 @@ minetest.register_node("civfoundations:plaster_white_h", {
 	paramtype2 = "facedir",
 	tiles = {"civf_plaster_white_h.png"},
 	is_ground_content = false,
-	groups = {cracky = 2, level = 1},
+	groups = {cracky = 2, crumbly = 1, level = 1},
 	sounds = default.node_sound_stone_defaults(),
 	node_dig_prediction = "solid_air",
 })
@@ -46,7 +54,7 @@ minetest.register_node("civfoundations:plaster_white_v", {
 	paramtype2 = "facedir",
 	tiles = {"civf_plaster_white_v.png"},
 	is_ground_content = false,
-	groups = {cracky = 2, level = 1},
+	groups = {cracky = 2, crumbly = 1, level = 1},
 	sounds = default.node_sound_stone_defaults(),
 	node_dig_prediction = "solid_air",
 })
@@ -78,8 +86,8 @@ minetest.register_node("civfoundations:marble_red", {
 minetest.register_node("civfoundations:mudbrick", {
 	description = "Mudbrick",
 	tiles = {"civf_mudbrick.png"},
-	groups = {cracky = 2, crumbly = 2, level = 1},
-	sounds = default.node_sound_stone_defaults(),
+	groups = {cracky = 2, crumbly = 1, level = 1},
+	sounds = default.node_sound_dirt_defaults(),
 	node_dig_prediction = "solid_air",
 })
 
@@ -113,4 +121,39 @@ minetest.register_node("civfoundations:slate_green", {
 	groups = {cracky = 2, crumbly = 1, level = 1},
 	sounds = default.node_sound_stone_defaults(),
 	node_dig_prediction = "solid_air",
+})
+
+--REGISTER RECIPES, PILLARS ETC
+
+minetest.register_craft({
+	output = "civfoundations:adobe 4",
+	type = "shaped",
+	recipe = {
+		{"default:dry_dirt", "default:dry_dirt"},
+		{"default:dry_dirt", "default:dry_dirt"}
+	}
+})
+
+minetest.register_craft({
+	output = "civfoundations:mudbrick 4",
+	type = "shaped",
+	recipe = {
+		{"default:dirt", "default:dirt"},
+		{"default:dirt", "default:dirt"}
+	}
+})
+
+pillars.register_pillar("pillars:marble_green", {
+	description = "Green Marble Pillar",
+	textures = {"civf_marble_green.png"},
+	sounds = sound,
+	groups = {snappy=2, cracky=3, oddly_breakable_by_hand=3},
+	basenode = "civfoundations:marble_green"
+})
+pillars.register_pillar("pillars:marble_red", {
+	description = "Red Marble Pillar",
+	textures = {"civf_marble_red.png"},
+	sounds = sound,
+	groups = {snappy=2, cracky=3, oddly_breakable_by_hand=3},
+	basenode = "civfoundations:marble_red"
 })
